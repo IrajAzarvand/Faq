0db1c0a141bdf08516596f04e481eae0c1498e80
Iraj Azarvand <iraj.azarvand@gmail.com>
2019-09-11 13:23:56 +0430

    modified faq file

--- src/views/faq.blade.php
+++ src/views/faq.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
     <article>
+        <h1>لیست سوالات متداول</h1>
         @foreach($data as $it)
         <h2><a href="/faq/{{$it->id}}"> {{$it->q}}</a></h2>
         <p>{{$it->a}}</p>
